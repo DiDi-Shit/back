79f17430382f5c4faf5a672a9fc2cae0469ab472
yangjiangecs <53853932+yangjiangecs@users.noreply.github.com>
2020-05-08 17:46:56 +0800

    Add files via upload

---
+++ module/_init_.py
@@ -0,0 +1,41 @@
+import asyncpg
+import base64
+from cryptography import fernet
+from aiohttp import web
+from aiohttp_session import setup
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
+from utils import *
+import os.path as path
+from .login import signup, signin
+from .getToilet import getToilet
+from .addToilet import addToilet
+from .sample import *
+from .paperToilet import paperToilet，Delete_Order，Update_Order，Query_Order
+
+
+async def initapp(config):
+    app = web.Application()
+    fernet_key = fernet.Fernet.generate_key()
+    secret_key = base64.urlsafe_b64decode(fernet_key)
+    setup(app, EncryptedCookieStorage(secret_key))
+
+    app["config"] = load_config(config)
+    dbconfig = app["config"]["db"]
+
+    app["pool"] = await asyncpg.create_pool(
+        user=dbconfig["user"],
+        database=dbconfig["database"],
+        password=dbconfig["password"],
+    )
+
+    app.router.add_get("/", hello)
+    app.router.add_post("/login", signin)
+    app.router.add_post("/signup", signup)
+    app.router.add_post("/getToilet", getToilet)
+    app.router.add_post("/addToilet", addToilet)
+    app.router.add_post("/Create_Order", Create_Order)
+    app.router.add_post("/Delete_Order", Delete_Order)
+    app.router.add_post("/Update_Order", Update_Order)
+    app.router.add_post("/Query_Order", Query_Order)
+
+    return app
